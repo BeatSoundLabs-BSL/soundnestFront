30120a9cae289d5349ff6681a959c668c4a40932
JJSON-11110 <millonesjesus@outlook.com>
2025-05-13 02:04:19 -0500

    feat(ui): add base form component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {LanguageSwitcherComponent} from './public/components/language-switcher/language-switcher.component';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LanguageSwitcherComponent, MatToolbarRow, MatToolbar, MatAnchor, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
