ed400bc8846796991bcea6e8b97cd8b0f816e042
JJSON-11110 <millonesjesus@outlook.com>
2025-05-13 00:27:26 -0500

    feat(public): add reservation page component.

--- src/app/public/pages/Dashboard/dashboard.component.ts
+++ src/app/public/pages/Dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-Dashboard',
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
--- src/app/soundnest/components/reservation-summary/reservation-summary.component.ts
+++ src/app/soundnest/components/reservation-summary/reservation-summary.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-reservation-summary',
+  selector: 'app-Reservation-summary',
   imports: [],
   templateUrl: './reservation-summary.component.html',
   styleUrl: './reservation-summary.component.css'
